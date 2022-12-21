c20d51f555f5c1b95c23e0b90e299b2219fd75d8
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2022-12-21 18:17:54 -0300

    atualização

---
+++ Assets/editorHtml/README.md
@@ -0,0 +1,40 @@
+# MESH
+> A sleek, collaborative, online code editor for HTML, CSS, JavaScript
+
+---
+
+This project won Firebase's solo hackathon when their API was first exposed. That said, it will continue to grow (and be updated) with new features. If something is not working or you would like a new feature, please use the issues page.
+
+## Demo
+
+Click for a demo: <a href="http://farhadg.github.io/mesh-code-editor" target="_blank">Mesh Code Editor</a>.
+
+## Installation
+
+You can simply fork and clone (or download) Mesh into your local directory:
+
+```
+  $ git clone https://www.github.com/farhadg/mesh-code-editor
+```
+
+## Usage
+
+Fire up `index.html` for the online code editor. You will be presented with some default text that'll provide you an idea of the layout.
+
+The preview box can be adjusted and dragged around. Be sure to select and drag around the edges (handlers), since the contents within the frame are exempt of this behaviour.
+
+You can also switch between a dark or light theme.
+
+For basic editor modifications, checkout `js/app.js` and `css/style.css`.
+
+<table>
+<tr>
+<td>HTML</td>
+<td>CSS</td>
+<td>JavaScript (jQuery)</td>
+</tr>
+</table>
+
+## Options
+
+I'll be adding more meshing features; that said, if you'd like a feature, let me know so that I'll try and implement it into future updates.
--- Pages/script.php
+++ Pages/script.php
@@ -9,21 +9,50 @@
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                               
-                            
+                             <!--------------------------------------------------->  
+                             
+                             <link rel="stylesheet" href="./Assets/editorHtml/css/reset.css" type="text/css">
+                            <link rel="stylesheet" href="./Assets/editorHtml/css/style.css" type="text/css">
+                            <script src="https://cdn.firebase.com/v0/firebase.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/codemirror.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/css.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/htmlmixed.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/xml.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/javascript.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/jquery.js"></script>
+                            <script src="./Assets/editorHtml/js/vendor/jquery-ui.js"></script>
+
+                            <script src="./Assets/editorHtml/js/app.js"></script>
+                            </head>
+                            <body class="cm-s-default">
+
+                            <div id="controls">
+                                <!-- <a class="button together button-on" onclick="TogetherJS(this); return false;" title="toggle collaboration"></a> -->
+                                <a href="#" class="button lights" title="toggle the lights"></a>
+                            </div>
+
+                            <textarea id="html">HTML</textarea>
+                            <textarea id="css">CSS</textarea>
+                            <textarea id="js">JS</textarea>
+
+                            <div id="frame" class="cm-s-default frame">
+                                <iframe id="preview"></iframe>
+                            </div>
 
 
 
 
 
 
-                            
+                            <!--------------------------------------------------->  
                             </div>
                         </div>
                     </div>
 
                 </div>
-                <!-- /.container-fluid -->
+                <!-- /.container-fluid 
+                Assets\editorHtml
+            -->
 
             </div>
             <!-- End of Main Content -->
