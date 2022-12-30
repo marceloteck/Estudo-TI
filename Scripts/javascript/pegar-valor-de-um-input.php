077655a2427a2e33ee3e9105cd8b563ee0c97fd5
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2022-12-30 10:54:09 -0300

    Create pegar-valor-de-um-input.php

---
+++ Scripts/javascript/pegar-valor-de-um-input.php
@@ -0,0 +1,4 @@
+<!--startJs-->  <script> function GetInputValue(INput){
+  var INput1web = document.querySelector(INput).value;
+  return INput1web;
+}  </script> <!--endJs--><!-- /** --TITULO--:pegar valor de um input| --LINK--:./Scripts/javascript/pegar-valor-de-um-input.php| **/ -->
