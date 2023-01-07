3bbf93a5e43f9b0739cfbe326a97ded81ed76946
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2023-01-07 16:22:31 -0300

    atualização

--- Scripts/javascript/abrir-link-em-nnova-guia-com-js.php
+++
@@ -1,3 +0,0 @@
-<!--startJs-->  <script> function abrirUrl(link){
-  window.open(link,"_blank");
-} </script> <!--endJs--><!-- /** --TITULO--:abrir link em nnova guia com js| --LINK--:./Scripts/javascript/abrir-link-em-nnova-guia-com-js.php| **/ -->
