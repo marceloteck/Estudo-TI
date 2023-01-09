79d4da16abe0feb594572127ed74ffd3751fc849
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2023-01-09 08:01:43 -0300

    Update testes.php

--- Scripts/zona_do_php/testes.php
+++ Scripts/zona_do_php/testes.php
@@ -2,27 +2,10 @@
 //phpinfo();
 //echo $_SERVER['HTTP_USER_AGENT'];
 ?>
-<h1>texte de titulo</h1>
-<!DOCTYPE html>
-<html>
-<body>
-
-<form method="get" action="<?=$_SERVER['PHP_SELF']?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
-</form>
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // collect value of input field
-    $name = htmlspecialchars($_REQUEST['fname']);
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo $name;
-    }
-}
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
 ?>
-
-</body>
 </html>
